7503dcd18e73a9893f14c99ab36882dd02063506
samdravitzki <samdravitzki@gmail.com>
2019-01-23 19:55:20 +1300

    Updates

---
+++ precache-manifest.17b9cb9654413aa941e50e4ab4ecbd4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "b7a67aa84130ce9b149d",
+    "url": "/react-portfolio/static/js/main.b7a67aa8.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/react-portfolio/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "b7a67aa84130ce9b149d",
+    "url": "/react-portfolio/static/css/main.a274ef66.chunk.css"
+  },
+  {
+    "revision": "526e3f242ecd4287c7698c1b07ba990b",
+    "url": "/react-portfolio/index.html"
+  }
+];
